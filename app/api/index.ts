4371dcd128f6661481e6c5c1c3695bcdf9d5254d
kietn20 <kietnguyen3698@gmail.com>
2024-01-20 10:48:01 -0800

    fixing puppeteer x53, trying cdn again

--- app/api/index.ts
+++ app/api/index.ts
@@ -84,7 +84,7 @@ async function getBrowserInstance() {
 	return puppeteer.launch({
 		args: chromium.args,
 		defaultViewport: chromium.defaultViewport,
-		executablePath: await chromium.executablePath(),
+		executablePath: await chromium.executablePath('https://github.com/Sparticuz/chromium/releases/download/v119.0.2/chromium-v119.0.2-pack.tar'),
 		headless: chromium.headless,
 		ignoreHTTPSErrors: true,
 	});
