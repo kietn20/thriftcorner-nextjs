bc9e42eb5960e44b18e0d3c4ec7325c6024e7bb2
kietn20 <kietnguyen3698@gmail.com>
2024-01-20 15:48:09 -0800

    fixing puppeteer x71, added @sparticuz/chromium to externals

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ['mongoose', 'puppeteer-core']
+    serverComponentsExternalPackages: ['mongoose', 'puppeteer-core', '@sparticuz/chromium']
   },
   images: {
     domains: ['i.ebayimg.com']
