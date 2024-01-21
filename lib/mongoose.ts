2e8954216172847eb9e7a30ed4d4856d26a5a3aa
kietn20 <kietnguyen3698@gmail.com>
2024-01-21 14:39:39 -0800

    fixing puppeteer x100

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,23 +1,22 @@
 import mongoose from 'mongoose'
 
-let isConnected = false; // variable to track the connect status
+// let isConnected = false; // variable to track the connect status
 
 export const connectToDB = async () => {
     mongoose.set('strictQuery', true);
     
     if (!process.env.MONGODB_URI) return console.log('MONGODB_URL is not defined');
     
-    if (isConnected) return console.log('Using existing database connection')
+    // if (isConnected) return console.log('Using existing database connection')
     
     try {
         await mongoose.connect(process.env.MONGODB_URI);
         
-        isConnected = true
+        // isConnected = true
         console.log('MongoDB connected')
     } catch (error) {
         console.log(error)
-    } finally {
-    }
+    } 
 }
 
 export const deleteCollection = async () => {
