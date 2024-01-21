6d925e7991fe7b8f851ec5b06feb624f40ef8772
kietn20 <kietnguyen3698@gmail.com>
2024-01-20 19:39:41 -0800

    fixing puppeteer x84, trying to lower execution time

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -20,7 +20,7 @@ export async function scrapeAndStoreProduct(searchQuery: string) {
 				product,
 				{ upsert: true, new: true }
 			);
-			revalidatePath(`/products/${newProduct._id}`)
+			// revalidatePath(`/products/${newProduct._id}`)
 		});
 	} catch (error: any) {
 		throw new Error(`Failed to create/update product: ${error.message}`);
