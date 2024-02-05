3c8d8859e7e224b4b1a935e546962136e2b185c5
kietn20 <kietnguyen3698@gmail.com>
2024-02-04 19:28:39 -0800

    fixing puppeteer x131

--- app/api/index.ts
+++ app/api/index.ts
@@ -1,6 +1,6 @@
 // "use server";
-export const maxDuration = 9; // This function can run for a maximum of 5 seconds
-export const dynamic = 'force-dynamic';
+// export const maxDuration = 9; // This function can run for a maximum of 5 seconds
+// export const dynamic = 'force-dynamic';
 import Product from "@/lib/models/product.model";
 import { connectToDB } from "@/lib/mongoose";
 import mongoose, { mongo } from "mongoose";
